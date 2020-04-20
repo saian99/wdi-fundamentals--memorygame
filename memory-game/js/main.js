064b48017dd39ca526be3874e481490d85ac2180
Xavier Johnson <xavierjohnson@xaviers-air.home>
2020-04-19 22:00:46 -0700

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,18 +1,40 @@
-const cards = ['queen', 'queen', 'king', 'king']
+const cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 const cardsInPlay = []
 function checkForMatch() {
 	if (cardsInPlay[0]===cardsInPlay[1]){
 		console.log("You found a match!");
 	}
 	else if (cardsInPlay[0]!==cardsInPlay[1]) {
-		console.log("sorry, try again");
+		alert("sorry, try again");
 	}
 }
 function flipCard(cardId) {
-console.log("user flipped" + cards[cardId])
-cardsInPlay.push(cards[cardId]);
+console.log("user flipped" + cards[cardId].rank)
+cardsInPlay.push(cards[cardId].rank);
 if (cardsInPlay.length === 2) {checkForMatch()}
-
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
 }
 flipCard(0);
 flipCard(2);
