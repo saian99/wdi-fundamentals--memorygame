768540396b987d9a81fee985494302feca9c3e01
Xavier Johnson <xavierjohnson@xaviers-air.home>
2020-04-19 19:27:24 -0700

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,9 +1,20 @@
-console.log("up and running!");
+const cards = ['queen', 'queen', 'king', 'king']
 
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+const cardsInPlay = []
+const cardOne = cards[0]
+cardsInPlay.push(cardOne);
+console.log(cardOne)
 
-console.log("user flipped " + cardOne);
-console.log("user flipped " + cardThree);
+const cardTwo = cards[2]
+cardsInPlay.push(cardTwo);
+console.log(cardTwo)
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0]===cardsInPlay[1]){
+		alert("You found a match!");
+	}
+	else if (cardsInPlay[0]!==cardsInPlay[1]) {
+		alert("sorry, try again");
+	}
+}
+
