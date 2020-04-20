12d1163ed1eea928a51c410585e0405480456990
Xavier Johnson <xavierjohnson@xaviers-air.home>
2020-04-19 18:05:14 -0700

    created game logic

---
+++ memory-game/js/main.js
@@ -0,0 +1,9 @@
+console.log("up and running!");
+
+let cardOne = "queen";
+let cardTwo = "queen";
+let cardThree = "king";
+let cardFour = "king";
+
+console.log("user flipped " + cardOne);
+console.log("user flipped " + cardThree);
